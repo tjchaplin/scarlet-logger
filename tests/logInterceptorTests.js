f06672402357dc868fb8061587b2934ec8c1b13a
Tim Chaplin <tjchaplin@hotmail.com>
2013-06-02 23:16:37 +0100

    Initial import

---
+++ tests/logInterceptorTests.js
@@ -0,0 +1,80 @@
+var should = require('should');
+var LogInterceptor = require("../lib/logInterceptor.js");
+
+function interceptorContext(){
+	var self = this;
+	self.logMessage = undefined;
+	self.error = undefined;
+	self.methodHasBeenCalled = false;
+	self.debugHasBeenCalled = false;
+	self.errorHasBeenCalled = false;
+
+	self.logInterceptor = new LogInterceptor();
+	self.logInterceptor.method = function(){self.methodHasBeenCalled = true;};
+	self.logInterceptor.debug = function(message){self.debugHasBeenCalled = true; self.logMessage = message;};
+	self.logInterceptor.error = function(error){self.errorHasBeenCalled = true; self.error = error;};
+
+	return self;
+};
+
+describe('Given using a log interceptor',function(){
+	describe('When has a method to log',function(){
+		var context = new interceptorContext();
+		function function1(){};
+		function1.prototype.add1 = function(value){
+			if(!value)
+				value = 1;
+			return value+1;
+		};
+
+		it("should call debug log",function(onComplete){
+			var objectToLog = new function1();	
+			context.logInterceptor.functionInvocation(objectToLog.add1);
+			context.debugHasBeenCalled.should.be.eql(true);
+			onComplete();
+		});
+	});
+	describe('When has a method to log with parameters',function(){
+		var context = new interceptorContext();
+		function function1(){};
+		function1.prototype.add1 = function(value){
+			if(!value)
+				value = 1;
+
+			return value+1;
+		};
+
+		it("should call debug log",function(onComplete){
+			var objectToLog = new function1();	
+			var result = context.logInterceptor.functionInvocation(objectToLog.add1,2);
+			result.should.be.eql(3);
+			onComplete();
+		});
+	});
+	describe('When method to log throws an exception',function(){
+		var context = new interceptorContext();
+		var errorFunction = function(){throw new Error("Something bad happened.") };
+		var didThrowError = false;
+		var thrownException;
+		try{
+			context.logInterceptor.functionInvocation(errorFunction);
+		}catch(exception){
+			didThrowError = true;
+			thrownException = exception;
+		}	
+		it("should throw error",function(onComplete){
+			didThrowError.should.be.eql(true);
+			onComplete();
+		});
+
+		it("should call error log",function(onComplete){
+			context.errorHasBeenCalled.should.be.eql(true);
+			onComplete();
+		});
+
+		it("error should be in error message",function(onComplete){
+			context.error.toString().indexOf(thrownException).should.be.greaterThan(-1);
+			onComplete();
+		});
+	});
+});
---
+++ tests/scarletLoggerTests.js
@@ -0,0 +1,53 @@
+var should = require('should');
+var ScarletLogger = require("../lib/scarlet-logger.js");
+
+var TestAppender = function(){ this.didAppend = false; };
+TestAppender.prototype.append = function(message){this.didAppend = true; this.message = message;} 	
+
+describe('Given using a Aspect Logger',function(){
+	describe('When creating logging aspect',function(){
+		it("should return method results without modification",function(onComplete){
+			ScarletLogger.unBindLogger();
+
+			function ObjectToLog(){};
+			ObjectToLog.prototype.someMethod = function(){return 1;};
+			var testAppender = new TestAppender();
+			ScarletLogger.appender(testAppender).bindLogger(ObjectToLog);
+			var objectToLog = new ObjectToLog();
+			var result = objectToLog.someMethod();
+			result.should.be.eql(1);
+			onComplete();
+		});
+	});
+	describe('When removing the bound logger',function(){
+		it("should not log",function(onComplete){
+			ScarletLogger.unBindLogger();
+
+			function ObjectToLog(){};
+			ObjectToLog.prototype.someMethod = function(){return 1;};
+			var testAppender = new TestAppender();
+			ScarletLogger.appender(testAppender).bindLogger(ObjectToLog);
+			ScarletLogger.unBindLogger();
+
+			var objectToLog = new ObjectToLog();
+			objectToLog.someMethod();
+			testAppender.didAppend.should.be.eql(false);
+			onComplete();
+		});
+	});
+	describe('When binding a logger',function(){
+		it("should log",function(onComplete){
+			ScarletLogger.unBindLogger();
+
+			function ObjectToLog(){};
+			ObjectToLog.prototype.someMethod = function(){return 1;};
+			var testAppender = new TestAppender();
+			ScarletLogger.appender(testAppender).bindLogger(ObjectToLog);
+
+			var objectToLog = new ObjectToLog();
+			objectToLog.someMethod();
+			testAppender.didAppend.should.be.eql(true);
+			onComplete();
+		});
+	});
+});
